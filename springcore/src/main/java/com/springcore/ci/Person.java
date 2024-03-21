03a485af91309b9c4abd67bcdb29fc5a08c4fc00
md-saif7474 <mdsaifgzp33@gmail.com>
2024-03-22 01:33:50 +0530

    Constructor Injection

--- springcore/src/main/java/com/springcore/ci/Person.java
+++ springcore/src/main/java/com/springcore/ci/Person.java
@@ -2,17 +2,19 @@ package com.springcore.ci;
 
 public class Person {
 	private String name;
-	private String personId;
+	private int personId;
+	private Certi certi;
 	
-	public Person(String name, String personId)
+	public Person(String name, int personId,Certi certi)
 	{
 		this.name = name;
 		this.personId = personId;
+		this.certi = certi;
 	}
 
 	@Override
 	public String toString() {
-		return this.name + " : " + this.personId ;
+		return this.name + " : " + this.personId + " { " + this.certi.name + " } " ;
 	}
 	
 	
